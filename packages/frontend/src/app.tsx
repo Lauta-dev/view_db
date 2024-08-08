a8f92a1c84b171e4152571d52d2331a7b321c89b
lauta <altairdiaz245@gmail.com>
2024-08-08 14:16:35 -0300

    refactor(frontend): improve data fetching and error handling in app.tsx
    Enhanced error handling in data fetching logic.
    Updated state management to use specific types.
    Improved table rendering logic and moved inline logic to separate functions.

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -3,18 +3,32 @@ import GetTable from './components/getTables'
 import { localUrl } from './cons'
 import "./css/container.css"
 import "./css/table.css"
+import { ReturnData, Server } from './interface/server.interface'
 
-// TODO: URGENTE: Arreglar los estilos de esta app de mierda
+// TODO: Arreglar los estilos de esta app de mierda
 
 export function App() {
   const [selectRow, setSelectRow] = useState<string>()
-  const [m, setM] = useState<[]>()
+  const [data, setData] = useState<ReturnData>()
+  const [errorInFetch, setErrorInFetch] = useState<boolean>(false)
 
   useEffect(() => {
     async function getColumns() {
-      const f = await fetch(`${localUrl}/t?table=${selectRow}`)
-      const json = await f.json()
-      setM(json)
+      setErrorInFetch(false);
+
+      try {
+        const fetchData = await fetch(`${localUrl}/t?table=${selectRow}`)
+        const { server }: Server = await fetchData.json()
+
+        if (server.statusCode !== 200) {
+          setErrorInFetch(true)
+          return 0
+        }
+
+        setData(server.returnData)
+      } catch (error) {
+        setErrorInFetch(true)
+      }
     }
 
     selectRow ? getColumns() : null
@@ -30,48 +44,50 @@ export function App() {
         <GetTable children={choiseTable} />
 
         <div className="t-container">
-          <table>
-            <thead>
-              <tr>
-                {m?.a.map(data => (
-                  <th key={data.columnName}> {data.columnName} ({data.dataType})</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="tt">
-                {m?.a.map(e => (
-                  <td  key={e.INTERNAL_UUID}>
-                    {m?.reg.map(data => {
-                      // TODO: Mover esto a otro archivo
-                      if (e.dataType === "date" || e.dataType === "timestamp with time zone") {
-                        const date = new Date(data[e.columnName]);
-                        const options = {
-                          year: 'numeric',
-                          month: '2-digit',
-                          day: '2-digit',
-                          hour: 'numeric',
-                          minute: 'numeric',
-                          second: 'numeric',
-                          timeZoneName: 'short'
-                        };
+          {errorInFetch ? "no" : <>
+            <table>
+              <thead>
+                <tr>
+                  {data?.tableInfo.map(data => (
+                    <th key={data.columnName}> {data.columnName} ({data.dataType})</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="tt">
+                  {data?.tableInfo.map(e => (
+                    <td key={e.UUID}>
+                      {data?.registers.map(data => {
+                        // TODO: Mover esto a otro archivo
+                        if (e.dataType === "date" || e.dataType === "timestamp with time zone") {
+                          const date = new Date(data[e.columnName]);
+                          const options = {
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit',
+                            hour: 'numeric',
+                            minute: 'numeric',
+                            second: 'numeric',
+                            timeZoneName: 'short'
+                          };
 
-                        const formatter = new Intl.DateTimeFormat('en-US', options);
-                        const formattedDate = formatter.format(date);
+                          const formatter = new Intl.DateTimeFormat('en-US', options);
+                          const formattedDate = formatter.format(date);
 
-                        data[e.columnName] = formattedDate
+                          data[e.columnName] = formattedDate
+                        }
+
+                        if (!data[e.columnName]) data[e.columnName] = "-"
+                        return <p key={data[e.columnName]}>{data[e.columnName].toString()}</p>
                       }
 
-                      if (!data[e.columnName]) data[e.columnName] = "-"
-                      return <p key={data.INTERNAL_UUID}>{data[e.columnName].toString()}</p>
-                    }
-
-                    )}
-                  </td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              </tbody>
+            </table>
+          </>}
         </div>
       </div>
 
